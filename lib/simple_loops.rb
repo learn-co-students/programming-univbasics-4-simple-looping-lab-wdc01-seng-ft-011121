47200aa80e64c1faabab556fcca50243b9a12639
Fem Adisa <femiwonderful@gmail.com>
2020-11-06 01:11:58 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  5.times do 
+    puts string
+  end
+end
+
+def loop_message_n_times(string, int)
+  int.times do
+    puts string 
+  end
+end
+
+def output_array(array)
+  for i in array do
+    puts i
+  end
+end
+
+def return_string_array(array)
+  new_array = Array.new 
+  for i in array do
+    new_array.push(i.to_s)
+  end
+  return new_array
+end
